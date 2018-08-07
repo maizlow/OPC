16d7e8733046d40e12d9e51352f18a6de3a47d0d
maizlow <johan@maizlow.se>
2018-08-07 03:11:26 +0200

    blä

--- Webserver/app.js
+++ Webserver/app.js
@@ -1,2 +1,40 @@
+/**
+ * Module dependencies.
+ */
 
-console.log ("testar");
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , EmployeeProvider = require('./employeeprovider').EmployeeProvider;
+
+var app = express();
+
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {layout: false});
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(require('stylus').middleware(__dirname + '/public'));
+    app.use(express.static(path.join(__dirname, 'public')));
+  });
+
+  app.configure('development', function(){
+    app.use(express.errorHandler());
+  });
+
+
+//   Code goes here
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000);
